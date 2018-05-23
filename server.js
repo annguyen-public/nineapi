e6c61e8c7a4216b04cad09d9ceb47d12aeaab0a9
AnNguyen <nguyenan.public@gmail.com>
2018-05-23 19:05:44 +0700

    add db

--- server.js
+++ server.js
@@ -14,19 +14,19 @@ async function memes() {
     }
 }
 
-/*var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://nineuser:123@ds133550.mlab.com:33550/nine_db";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   console.log("Database created!");
   db.close();
-});*/
+});
 
 var i = 0;
-var myInt = setInterval(function () {
+/*var myInt = setInterval(function () {
     i++;
-}, 2000);
+}, 2000);*/
 
 app.get('/listUsers', function (req, res) {
 	memes();
